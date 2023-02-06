ce2d209e9fa421084465d4ce5ed8b79d7b58165a
s0f0 <sopho.papiashvili01@gmail.com>
2023-02-06 14:03:09 +0400

    add forms page in addresume file with style; add js file for the quality selection section

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('#button');
+const select = document.querySelector('#dropdown');
+const options = document.querySelectorAll('.option');
+const selectLabel = document.querySelector('#select-label');
+
+button.addEventListener('click', function(e){
+    e.preventDefault();
+    toggleHidden();
+})
+
+function toggleHidden(){
+    select.classList.toggle('hidden')
+}
+
+options.forEach(function(option){
+    option.addEventListener('click', function(e){
+        setSelectTitle(e);
+    })
+})
+
+function setSelectTitle(e){
+    const labelElement = document.querySelector(`label[for="${e.target.id}"]`).innerHTML;
+    selectLabel.innerHTML = labelElement;
+    selectLabel.style.color = "#1A1A1A";
+    toggleHidden();
+}
