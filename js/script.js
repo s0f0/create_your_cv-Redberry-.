582491f4256fa324fb1b6ffff56fd584de267335
s0f0 <sopho.papiashvili01@gmail.com>
2023-02-08 21:26:03 +0400

    add next and back button and add  photo functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// form drop down for quality
+
 const button = document.querySelector('#button');
 const select = document.querySelector('#dropdown');
 const options = document.querySelectorAll('.option');
@@ -23,4 +25,53 @@ function setSelectTitle(e){
     selectLabel.innerHTML = labelElement;
     selectLabel.style.color = "#1A1A1A";
     toggleHidden();
+}
+
+// show upload img
+
+const uploadBtn = document.getElementById("upload-btn");
+const chosenImage = document.getElementById("chosen_image");
+
+
+uploadBtn.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadBtn.files[0]);
+    console.log(uploadBtn.files[0]);
+    reader.onload = () => {
+        chosenImage.setAttribute("src", reader.result);
+    }
+}
+
+
+
+// change forms page
+
+const steps = Array.from(document.querySelectorAll('form .step'));
+const nextBtn = document.querySelectorAll('form .next-btn');
+const prevBtn = document.querySelectorAll('form .previous-btn');
+const form = document.querySelector('form');
+
+nextBtn.forEach(button=>{
+    button.addEventListener('click', ()=> {
+        changeStep('next');
+    })
+})
+prevBtn.forEach(button=> {
+    button.addEventListener('click', ()=> {
+        changeStep('prev')
+    })
+})
+
+function changeStep(btn) {
+    let index = 0;
+    const active = document.querySelector('form .step.active');
+    index = steps.indexOf(active);
+    steps[index].classList.remove('active');
+    if(btn === 'next'){
+        index++;
+    }else if(btn === 'prev'){
+        index--;
+    }
+    steps[index].classList.add('active');
+    console.log(index);
 }
