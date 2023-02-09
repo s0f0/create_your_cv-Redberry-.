b93bce60b324bac69db5a46077e718e7e29c51c3
s0f0 <sopho.papiashvili01@gmail.com>
2023-02-10 00:43:45 +0400

    Add file fillCv.js; Add function resume filling in real time

---
+++ js/fillCv.js
@@ -0,0 +1,87 @@
+// fill cv ------------------------------------------------------
+// page 1
+const firstname = document.querySelector('#fname');
+const last_Name = document.querySelector('#lname');
+const personEmail = document.querySelector('#email');
+const PersonPhone = document.querySelector('#tel');
+const AboutMe = document.getElementById('about_Me');
+// page 2
+const CVposition = document.querySelector('#position');
+const workPlace = document.querySelector('#employer');
+const WstartDate = document.querySelector('#start_date');
+const WendDate = document.querySelector('#end_date');
+const AboutWork = document.querySelector('#about_work');
+// const f2NextBtn = document.querySelector('.f2_nextbtn');
+// page 3
+const eduSpace = document.querySelector('#education_speace');
+const eduEndDate = document.getElementById("edu-end-date");
+const aboutEdu = document.getElementById("about-edu");
+
+// personal information ------------------
+// first name
+firstname.addEventListener('keyup', () => {
+    document.getElementById("person_fname").innerHTML = firstname.value + " " ;
+})
+// last name
+last_Name.addEventListener('keyup', () =>{
+    document.getElementById("person_lname").innerHTML = last_Name.value;
+})
+// email
+personEmail.addEventListener('keyup', () =>{
+    document.getElementById("p_email").innerHTML = personEmail.value;
+    document.getElementById("email_icon").style.display = "block";
+})
+// phone number
+PersonPhone.addEventListener('keyup', () =>{
+    document.getElementById("Person_phone").innerHTML = PersonPhone.value;
+    document.getElementById("Phone_icon").style.display = "block";
+})
+//  about me 
+AboutMe.addEventListener('keyup', () => {
+    document.getElementById("about_me").innerHTML = AboutMe.value;
+    document.querySelector('.section_h').style.display = "block";
+    document.querySelector('.cvhr1').style.display = "block";
+})
+
+// experience --------------
+// position
+CVposition.addEventListener('keyup', () =>{
+    document.getElementById('Position').innerHTML = CVposition.value;
+    document.querySelector('.Work_experience').style.display = "block";
+})
+// work speace/employer
+workPlace.addEventListener('keyup', () => {
+    document.getElementById("Work_place").innerHTML = ", " + workPlace.value;
+})
+// work date
+
+// start date
+WstartDate.addEventListener('change', () => {
+    document.getElementById("start-date").innerHTML = WstartDate.value;
+})
+// end date
+WendDate.addEventListener('change', () => {
+    document.getElementById("end-date").innerHTML = " - " + WendDate.value;
+})
+// about work
+AboutWork.addEventListener('keyup', () => {
+    document.getElementById("aboutwork").innerHTML = AboutWork.value;
+    document.querySelector('.cvhr2').style.display = "block";
+})
+
+// education -----------------------
+
+eduSpace.addEventListener('keyup', () => {
+    document.getElementById("edu-spece").innerHTML = eduSpace.value;
+    document.querySelector('.education-h').style.display = "block";
+})
+// quality speac filling is written in script.js 
+
+// education date 
+eduEndDate.addEventListener('change', () => {
+    document.getElementById("edu_date").innerHTML = eduEndDate.value;
+})
+// about aducation
+aboutEdu.addEventListener('keyup', () => {
+    document.getElementById("aboutedu").innerHTML = aboutEdu.value;
+})
--- js/script.js
+++ js/script.js
@@ -24,6 +24,8 @@ function setSelectTitle(e){
     const labelElement = document.querySelector(`label[for="${e.target.id}"]`).innerHTML;
     selectLabel.innerHTML = labelElement;
     selectLabel.style.color = "#1A1A1A";
+    // fill cv
+    document.getElementById("edu-quality").innerHTML = ", " + selectLabel.innerHTML;
     toggleHidden();
 }
 
@@ -31,7 +33,7 @@ function setSelectTitle(e){
 
 const uploadBtn = document.getElementById("upload-btn");
 const chosenImage = document.getElementById("chosen_image");
-
+document.getElementById("personimg").style.display = "none";
 
 uploadBtn.onchange = () => {
     let reader = new FileReader();
@@ -40,6 +42,7 @@ uploadBtn.onchange = () => {
     reader.onload = () => {
         chosenImage.setAttribute("src", reader.result);
     }
+    document.getElementById("personimg").style.display = "block";
 }
 
 
@@ -75,3 +78,138 @@ function changeStep(btn) {
     steps[index].classList.add('active');
     console.log(index);
 }
+
+
+// აიდიებია გასასწორებელი ქაუნთერ ცვლადით დიდი ალბათობით ------------
+// add more experience
+const experienceDiv = document.querySelector('.experience');
+const experiensInCv = document.querySelector('.work');
+const AddExperience = document.querySelector('.exper_1');
+
+function addExperience() {
+    experienceDiv.insertAdjacentHTML('afterend', experienceDiv.innerHTML);
+    experiensInCv.insertAdjacentHTML('afterend', AddExperience.innerHTML);
+}   
+
+// აიდიებია გასასწორებელი ქაუნთერ ცვლადით დიდი ალბათობით -----------
+//   add more education
+const educationDIv = document.querySelector('.education');
+const educationInCv = document.querySelector('.educationCv');
+const AddAducation = document.querySelector('.educ_1');
+
+function addEducation() {
+    educationDIv.insertAdjacentHTML('afterend', educationDIv.innerHTML);
+    educationInCv.insertAdjacentHTML('afterend', AddAducation.innerHTML);
+}
+
+
+
+
+// filling out the CV
+
+// page 1 info
+
+const fname = document.querySelector('#fname');
+const lastName = document.querySelector('#lname');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#tel');
+const f1NextBtn = document.querySelector('.f1_nectbtn');
+// page 2 info
+
+const position = document.querySelector('#position');
+const employer = document.querySelector('#employer');
+const startDate = document.querySelector('#start_date');
+const endDate = document.querySelector('#end_date');
+const aboutWork = document.querySelector('#about_work');
+const f2NextBtn = document.querySelector('.f2_nextbtn');
+
+// page 3 info
+
+const aducationSpace = document.querySelector('#education_speace');
+
+
+// page 1 validation
+
+// f1NextBtn.addEventListener('click', (e) => {
+//     let Error = [];
+//     let res = /^[ა-ჰ]+$/.test(fname.value);
+//     // first and last name validation
+//     minTwoSymb(fname);
+//     minTwoSymb(lastName);
+//     function minTwoSymb(x){
+//         if(x.value.length < 2 || x.value === ''|| res === false){
+//             x.style.borderColor = "#EF5050";
+//             let labelClass = '.' + x.id + '_name';
+//             console.log(labelClass)
+//             console.log(x.id);
+//             document.querySelector(labelClass).style.color = "#EF5050";
+//             Error.push('Error');
+//         }else{
+//             x.style.borderColor = "#98E37E";
+//         }
+//     }
+//     // upload photo validation
+//     if(document.querySelector('#upload-btn').value == ''){
+//         document.querySelector('#upload_text').style.color = "#EF5050";
+//         document.querySelector('.warningicon').style.display = "block";
+//         Error.push('Error');
+//     }else{
+//         document.querySelector('.uploaded').style.display = "block";
+//     }
+
+//     // email validation
+
+//     let checkEmail = /^[a-zA-Z0-9.]+@redberry.ge$/.test(email.value);
+//     console.log(checkEmail);
+//     if(email.value === '' || checkEmail === false){
+//         email.style.borderColor = "#EF5050";
+//         document.querySelector('.labl_email').style.color = "#EF5050";
+//         Error.push('Error');
+//     }else{
+//         email.style.borderColor = "#98E37E";
+//     }
+
+//     // phone validation
+//     // რეჯექსია გასასწორებელი ყოველთვის ფოლსია
+//     let chackPhone = phone.value.match(/^\+995\s5\d{2}\s\d{2}\s\d{2}\s\d{2}$/);
+//     console.log(chackPhone);
+//     if(phone.value === ''|| chackPhone === false){
+//         phone.style.borderColor = "#EF5050";
+//         document.querySelector('.phone-lab').style.color = "#EF5050";
+//         Error.push('Error');
+//     }else{
+//         phone.style.borderColor = "#98E37E";
+//     }
+
+//     // button validation
+//     if(Error.length > 0){
+//         // button.addEventListener('click', function(e){
+//         //     e.preventDefault();
+//         // })
+//         steps.indexOf(document.querySelector('form .step.active')) = 0;
+//     }
+// })
+
+
+// page 2 validation
+// f2NextBtn.addEventListener('click', (e) => {
+//     let F2error = [];
+
+//     if(position.value === ''|| position.value.length < 2){
+//         position.style.borderColor = "#EF5050";
+//         document.querySelector('.position-labl').style.color = "#EF5050";
+//     }else{
+//        position.style.borderColor = "#98E37E"; 
+//     }
+ 
+//     // button validation
+//     if(F2error.length > 0){
+//         nextBtn.addEventListener('click', function(e){
+//             e.preventDefault();
+//         })
+//     }
+// })
+
+
+
+
